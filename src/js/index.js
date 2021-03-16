295d44fc9b53c5e3b0624a668b83d51aa7bb6201
sometimespipe <andrespipehl@gmail.com>
2021-03-16 16:42:01 +0000

    added traffic lights component and style

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
@@ -8,8 +8,30 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+const TrafficLight = () => {
+	let [color, setColor] = useState(null);
+
+	return (
+		<>
+			<div className="top bg-dark"></div>
+			<div className="trafficlight">
+				<div
+					className={"light red " + (color == "red" ? "glow" : null)}
+					onClick={() => setColor("red")}></div>
+				<div
+					className={
+						"light yellow " + (color == "yellow" ? "glow" : null)
+					}
+					onClick={() => setColor("yellow")}></div>
+				<div
+					className={
+						"light green " + (color == "green" ? "glow" : null)
+					}
+					onClick={() => setColor("green")}></div>
+			</div>
+		</>
+	);
+};
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
